474395823017c464e5e83b380e4887091d1d3cc0
zahid.shaikh@deptagency.com <Prinkal.Dabre@teknopoint.local>
2024-09-09 17:02:10 +0530

    changes accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,27 +4,31 @@ export default function decorate(block) {
   const accordionItems = [];
 
   // Decorate each row
-  [...block.children].forEach((row) => {
-    // Decorate accordion item label
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    summary.append(...label.childNodes);
+  try {
+    [...block.children].forEach((row) => {
+      // Decorate accordion item label
+      const label = row.children[0];
+      const summary = document.createElement('summary');
+      summary.className = 'accordion-item-label';
+      summary.append(...label.childNodes);
 
-    // Decorate accordion item body
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
+      // Decorate accordion item body
+      const body = row.children[1];
+      body.className = 'accordion-item-body';
 
-    // Decorate accordion item
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    details.append(summary, body);
-    row.replaceWith(details);
+      // Decorate accordion item
+      const details = document.createElement('details');
+      details.className = 'accordion-item';
+      details.append(summary, body);
+      row.replaceWith(details);
 
-    // Store references to accordion items
-    accordionItems.open = true;
-    accordionItems.push(details);
-  });
+      // Store references to accordion items
+      accordionItems.open = true;
+      accordionItems.push(details);
+    });
+  } catch (error) {
+
+  }
 
   // Add event listeners to manage open state
   accordionItems.forEach((item) => {
