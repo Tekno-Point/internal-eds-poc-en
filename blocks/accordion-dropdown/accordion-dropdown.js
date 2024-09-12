8f7c5b5f1aca73e751df8aa6342861065d15d371
Prinkal <prinkal.dabre@deptagency.com>
2024-09-12 11:35:15 +0530

    accordion changes

--- blocks/accordion-dropdown/accordion-dropdown.js
+++ blocks/accordion-dropdown/accordion-dropdown.js
@@ -1,23 +1,53 @@
-/*
- * Accordion Block
- * Recreate an accordion
- * https://www.hlx.live/developer/block-collection/accordion
- */
+import { isDesktop } from '../header/header.js';
 
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    // decorate accordion item label
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    summary.append(...label.childNodes);
-    // decorate accordion item body
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
-    // decorate accordion item
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    details.append(summary, body);
-    row.replaceWith(details);
+  const accordionItems = [];
+
+  // Decorate each row
+  try {
+    [...block.children].forEach((row) => {
+      // Decorate accordion item label
+      const label = row.children[0];
+      const summary = document.createElement('summary');
+      summary.className = 'accordion-item-label';
+      summary.append(...label.childNodes);
+
+      // Decorate accordion item body
+      const body = row.children[1];
+      body.className = 'accordion-item-body';
+
+      // Decorate accordion item
+      const details = document.createElement('details');
+      details.className = 'accordion-item';
+      details.append(summary, body);
+      row.replaceWith(details);
+
+      // Store references to accordion items
+      accordionItems.open = true;
+      accordionItems.push(details);
+    });
+  } catch (error) {
+    console.log('Error', error);
+  }
+
+  // Add event listeners to manage open state
+  accordionItems.forEach((item) => {
+    if (isDesktop.matches && block.classList.contains('only-mobile-expandable')) {
+      item.disabled = true;
+      item.open = true;
+      item.ariaDisabled = true;
+    } else {
+      item.addEventListener('toggle', () => {
+        // If the accordion item is being opened
+        if (item.open) {
+          // Close all other accordion items
+          accordionItems.forEach((otherItem) => {
+            if (otherItem !== item) {
+              otherItem.open = false;
+            }
+          });
+        }
+      });
+    }
   });
 }
