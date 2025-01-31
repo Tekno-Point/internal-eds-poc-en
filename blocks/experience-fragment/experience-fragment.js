c4b0429b0eae2dc3f8520da6a9f44234853d9fcb
TanmayTeknoPoint <tanmay.vedpathak@teknopoint.in>
2025-01-31 15:15:00 +0530

    common css

--- blocks/experience-fragment/experience-fragment.js
+++ blocks/experience-fragment/experience-fragment.js
@@ -1,19 +1,17 @@
 export async function appendXF(block, xfPath) {
     try {
-        
         const myHeaders = new Headers();
-    
         myHeaders.append("Authorization", "Basic YWRtaW46YWRtaW4=");
         myHeaders.append("Cookie", "cq-authoring-mode=TOUCH");
-    
+
         const requestOptions = {
             method: "GET",
             headers: myHeaders,
             redirect: "follow"
         };
-    
+
         const resp = await fetch(xfPath);
-    
+
         if (resp.ok) {
             let str = await resp.text();
             const { location } = window;
@@ -31,28 +29,33 @@ export async function appendXF(block, xfPath) {
             div.innerHTML = str;
             div.querySelectorAll('link').forEach((link) => {
                 try {
-                    const newLink = document.createElement('link');
-                    newLink.href = link.href.replace('http://localhost:3000', 'https://stg1-website.hdfclife.tech');
-                    newLink.rel = 'stylesheet';
-                    document.head.append(newLink);
+                    const newLinkHref = link.href.replace('http://localhost:3000', 'https://stg1-website.hdfclife.tech');
+                    if (!document.querySelector(`link[href="${newLinkHref}"]`)) {
+                        const newLink = document.createElement('link');
+                        newLink.href = newLinkHref;
+                        newLink.rel = 'stylesheet';
+                        document.head.append(newLink);
+                    }
                 } catch (error) {
-                    console.error(error); // eslint-disable-line
+                    console.error(error);
                 }
             });
             block.append(div.querySelector('.root'));
-            div.querySelectorAll('script').forEach((link) => {
-                const exculdeLink = [
+            div.querySelectorAll('script').forEach((script, index) => {
+                const excludeLinks = [
                     '/clientlibs/granite/',
                     '/foundation/clientlibs',
                 ];
-                // debugger;
-                if (!exculdeLink.filter((clientLib) => link.src.includes(clientLib)).length) {
+                const newScriptSrc = script.src.replace('http://localhost:3000', 'https://stg1-website.hdfclife.tech');
+                
+                if (!excludeLinks.some(clientLib => newScriptSrc.includes(clientLib)) && 
+                    !document.querySelector(`script[src="${newScriptSrc}"]`)) {
                     try {
                         const newScript = document.createElement('script');
-                        newScript.src = link.src.replace('http://localhost:3000', 'https://stg1-website.hdfclife.tech');
+                        newScript.src = newScriptSrc;
                         newScript.type = 'text/javascript';
-    
                         document.body.append(newScript);
+                        console.log(index, newScriptSrc);
                     } catch (error) {
                         console.error(error); // eslint-disable-line
                     }
@@ -84,3 +87,11 @@ export default async function decorate(block) {
     // xfPath = 'https://stg1-website.hdfclife.tech' + path;
     await appendXF(block, xfPath);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('img[data-src]');
+    images.forEach((img) => {
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.removeAttribute('data-src'); // Optional: Remove the data-src attribute
+    });
+});
