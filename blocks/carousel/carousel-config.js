d4355cf80371969ecf5b922d49aac5f4597ea44f
rahul.gohil1 <rahul.gohil@deptagency.com>
2025-02-18 20:34:18 +0530

    change in a js and also css

--- blocks/carousel/carousel-config.js
+++ blocks/carousel/carousel-config.js
@@ -94,9 +94,42 @@ const threeSlidesOnly = {
     },
   },
 };
+const fiveSlidesOnly = {
+  loop: true,
+  slidesPerView: 5,
+  grabCursor: true,
+  autoplay: false,
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
+  navigation: {
+    // nextEl: swiperButtonNext,
+    // prevEl: swiperButtonPrev,
+  },
+  pagination: {
+    // el: swiperPagination,
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+    },
+    780: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1199: {
+      slidesPerView: 3,
+    },
+  },
+};
 export default {
   'one-slide-only': oneSlideOnly,
   'two-slides-only': twoSlidesOnly,
   'three-slides-only': threeSlidesOnly,
+  'five-Slides-only':fiveSlidesOnly,
   default: threeSlidesOnly,
 };
