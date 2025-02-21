d1ea6dbef5315b15463b26fa8829e8878e78e994
TanmayTeknoPoint <tanmay.vedpathak@teknopoint.in>
2025-02-21 18:56:19 +0530

    image css added

--- blocks/custom-card/custom-card.js
+++ blocks/custom-card/custom-card.js
@@ -1,78 +1,61 @@
 export default function decorate(block) {
-  [...block.children].forEach((card, index) =>{
+  [...block.children].forEach((card, index) => {
     if (block.classList.contains('hdfc-card-number')) {
       card.classList.add('number-card');
       card.firstElementChild.classList.add('blue-number');
-      card.firstElementChild.textContent = index+1;
+      card.firstElementChild.textContent = index + 1;
 
-      const readMoreButton = card.querySelector('.button')
-      
+      const readMoreButton = card.querySelector('.button');
       if (readMoreButton) {
         readMoreButton.addEventListener("click", (e) => {
           e.preventDefault();
-            const parentElement = e.target.parentElement;
-            const previousSibling = parentElement.previousElementSibling;
-            
-            previousSibling.classList.toggle('active');
-            if (e.target.textContent.includes('more') || e.target.textContent.includes('More')) {
-                e.target.textContent = 'Read Less';
-            } else {
-                e.target.textContent = 'Read More';
-            }
-     
+          const parentElement = e.target.parentElement;
+          const previousSibling = parentElement.previousElementSibling;
+
+          previousSibling.classList.toggle('active');
+          if (e.target.textContent.includes('more') || e.target.textContent.includes('More')) {
+            e.target.textContent = 'Read Less';
+          } else {
+            e.target.textContent = 'Read More';
+          }
         });
       }
     }
 
     if (block.classList.contains('hdfc-detail')) {
-
-      // Add event listener to each .button inside the element
-      const button = card.querySelector('.button')
-      debugger;
+      const button = card.querySelector('.button');
       if (button) {
         button.addEventListener("click", (e) => {
           e.preventDefault();
-            // Find the parent element of the button
-            const parentElement = e.target.parentElement;
-            // Find the previous sibling of the parent
-            const previousSibling = parentElement.previousElementSibling;
-            
-            // Toggle the 'active' class on the previous sibling
-            previousSibling.classList.toggle('active');
-            console.log(e.target.textContent === 'Readmore')
-            if (e.target.textContent === 'Readmore') {
-                e.target.textContent = '...Readmore';  // Change to '...Readmore' when clicked
-            } else {
-                e.target.textContent = 'Readmore';  // Change back to 'Readmore'
-            }
-      
+          const parentElement = e.target.parentElement;
+          const previousSibling = parentElement.previousElementSibling;
+
+          previousSibling.classList.toggle('active');
+          if (e.target.textContent === 'Readmore') {
+            e.target.textContent = '...Readmore';
+          } else {
+            e.target.textContent = 'Readmore';
+          }
         });
       }
     }
-  });
+    
+    if (block.classList.contains('hdfc-plan-card')) {
+      const picture = card.querySelector('picture');
+      const button = card.querySelector('.button-container a');
 
-  // const hdfc=document.querySelectorAll('.hdfc-detail');
-  // if(hdfc.length > 0){
-  //   hdfc.forEach((element) => {
-  //     // Add event listener to each .button inside the element
-  //     element.querySelector('.button').addEventListener("click", (e) => {
-  //       e.preventDefault();
-  //         // Find the parent element of the button
-  //         const parentElement = e.target.parentElement;
-  //         // Find the previous sibling of the parent
-  //         const previousSibling = parentElement.previousElementSibling;
-          
-  //         // Toggle the 'active' class on the previous sibling
-  //         previousSibling.classList.toggle('active');
-  //         console.log(e.target.textContent === 'Readmore')
-  //         if (e.target.textContent === 'Readmore') {
-  //             e.target.textContent = '...Readmore';  // Change to '...Readmore' when clicked
-  //         } else {
-  //             e.target.textContent = 'Readmore';  // Change back to 'Readmore'
-  //         }
-   
-  //     });
-  // });
-  // }
-  
+      if (picture && button) {
+        const img = picture.querySelector('img');
+        const newWrapper = document.createElement('div');
+        const newLink = document.createElement('a');
+        
+        newLink.href = button.href;
+        newLink.appendChild(img.cloneNode(true));
+        newWrapper.appendChild(newLink);
+        
+        card.innerHTML = '';
+        card.appendChild(newWrapper);
+      }
+    }
+  });
 }
--- blocks/eds-container/eds-container.js
+++ blocks/eds-container/eds-container.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-    var parentEL = block.closest('.eds-container-wrapper');
+    const parentEL = block.closest('.eds-container-wrapper');
     block.innerHTML = '';
     
     if (parentEL) {
@@ -8,6 +8,20 @@ export default function decorate(block) {
             let nextSibling = sibling.nextElementSibling;
             block.appendChild(sibling);
             sibling = nextSibling;
+        }
+    }
+    
+    if (block.classList.contains("image-only-container")) {
+        const images = block.querySelectorAll('img');
+        if (images.length === 2) {
+            const defaultWrapper = block.querySelector('.default-content-wrapper');
+            if (defaultWrapper) {
+                images[0].classList.add('desktop-only');
+                images[1].classList.add('mobile-only');
+                defaultWrapper.innerHTML = '';
+                defaultWrapper.appendChild(images[0]);
+                defaultWrapper.appendChild(images[1]);
+            }
         }
     }
 }
